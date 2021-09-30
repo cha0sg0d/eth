dc7fcdb7011ad777bc7f2efa1589df1716f69c1a
cha0sg0d <tony.goss@protonmail.com>
2021-09-29 22:15:02 -0400

    send dao eth, set owner

--- tasks/dao.ts
+++ tasks/dao.ts
@@ -34,6 +34,12 @@ async function dao(
   await daoPlayer.deployed();
 
   console.log(`deployed daoPlayer to ${daoPlayer.address}`);
+
+  // update daoPlayer owner to be player in game
+  console.log('dao owner is', await daoPlayer.owner());
+  const PLAYER_ADDRESS = '0x1c0f0af3262a7213e59be7f1440282279d788335';
+  await daoPlayer.setOwner(PLAYER_ADDRESS);
+  console.log('new dao owner is', await daoPlayer.owner());
   
   console.log('whitelist address in dao.ts', hre.contracts.WHITELIST_CONTRACT_ADDRESS);
 
@@ -41,12 +47,27 @@ async function dao(
   const core = new hre.ethers.Contract(hre.contracts.CORE_CONTRACT_ADDRESS, CORE_ABI, deployer) as DarkForestCore;
 
   console.log('deployer address', deployer.address);
-
-  await hre.run('whitelist:registerAddress', { address });
   
-  const dao = await core.players(daoPlayer.address);
-  console.log('dao is whitelisted?', await whiteList.isWhitelisted(daoPlayer.address));
+  const from = deployer.address;
+  const to = daoPlayer.address;
+  await hre.run('wallet:send', { from, to, value: 20, dry: false});
 
+  const balance = await hre.ethers.provider.getBalance(daoPlayer.address);
+  console.log('dao balance:', hre.ethers.utils.formatEther(balance));
+  // don't need to whitelist; just need to deploy dao.
+  
+  // this is the public key of the first generic player
+  // const playerAddress = '0x1c0f0af3262a7213e59be7f1440282279d788335'
+  // await hre.run('whitelist:registerAddress', { playerAddress });
+
+  // await hre.run('whitelist:registerAddress', { address });
+  
+  // const dao = await core.players(daoPlayer.address);
+  // console.log('dao pre init', dao);
+  // console.log('dao is whitelisted?', await whiteList.isWhitelisted(daoPlayer.address));
+  
+
+  
   //
     // await daoPlayerInit();
 };
