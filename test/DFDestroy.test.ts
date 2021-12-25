75ea422173b4e27fce85be372fc15d2ba210fac2
cha0sg0d <tony.goss@protonmail.com>
2021-12-24 18:04:46 -0800

    update destroy test

--- test/DFDestroy.test.ts
+++ test/DFDestroy.test.ts
@@ -20,18 +20,21 @@ import {
   SMALL_INTERVAL,
   SPAWN_PLANET_1,
   SPAWN_PLANET_2,
+  initializers
 } from './utils/WorldConstants';
 
 const { BigNumber: BN } = ethers;
 
-describe('move to enemy planet', function () {
+describe('DarkForestDestroy', function () {
   let world: World;
+  const destroyThresold = 2;
 
   beforeEach(async function () {
     world = await fixtureLoader(defaultWorldFixture);
 
     await world.user1Core.initializePlayer(...makeInitArgs(SPAWN_PLANET_1));
     await world.user2Core.initializePlayer(...makeInitArgs(SPAWN_PLANET_2));
+    world.contracts.core.changeDestroyThreshold(destroyThresold);
   });
 
   it('should conquer but not destroy planet if sufficient forces', async function () {
@@ -75,13 +78,12 @@ describe('move to enemy planet', function () {
 
   it('should find destroy threshold', async function () {
     await increaseBlockchainTime();
-    // TODO Set Destroy Threshold
 
     const gameConstants = await world.user1Core.gameConstants()
-    expect(gameConstants.DESTROY_THRESHOLD.toNumber()).to.equal(2);
+    expect(gameConstants.DESTROY_THRESHOLD.toNumber()).to.equal(destroyThresold);
   });
 
-  it('should destroy a planet if sufficient forces', async function () {
+  it('should destroy a planet if sufficient forces and emit destroy event', async function () {
     await increaseBlockchainTime();
 
     const planet2Id = SPAWN_PLANET_2.id;
@@ -103,18 +105,14 @@ describe('move to enemy planet', function () {
     await world.user1Core.move(
     ...makeMoveArgs(SPAWN_PLANET_1, SPAWN_PLANET_2, dist, 70000, silverSent)
     )
-
-    await world.contracts.core.refreshPlanet(planet2Id);
-    const planet2Ext = await world.contracts.core.planetsExtendedInfo(planet2Id);
-    expect(planet2Ext.destroyed).to.equal(true);
-      
+    
     await expect(
-      world.user1Core.move(
-        ...makeMoveArgs(SPAWN_PLANET_1, SPAWN_PLANET_2, dist, 70000, silverSent)
-      )
+      world.contracts.core.refreshPlanet(planet2Id)
     )
     .to.emit(world.user1Core, "PlanetDestroyed")
     .withArgs(world.user1.address, SPAWN_PLANET_2.id);
-    
+ 
+    const planet2Ext = await world.contracts.core.planetsExtendedInfo(planet2Id);
+    expect(planet2Ext.destroyed).to.equal(true);
   });
 });
