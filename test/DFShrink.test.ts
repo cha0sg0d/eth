1bfcb0cc7cf0c03d6c40088dd0a5d9208d3088c7
cha0sg0d <tony.goss@protonmail.com>
2021-12-25 14:58:30 -0800

    clean up DFShrink

--- test/DFShrink.test.ts
+++ test/DFShrink.test.ts
@@ -7,12 +7,14 @@ import {
   increaseBlockchainTime,
   makeInitArgs,
   makeMoveArgs,
+  getCurrentTime
 } from './utils/TestUtils';
 import { defaultWorldFixture, growingWorldFixture, shrinkingWorldFixture, World } from './utils/TestWorld';
 import {
   LVL0_PLANET_OUT_OF_BOUNDS,
   LVL0_PLANET_POPCAP_BOOSTED,
   LVL1_PLANET_SPACE_FURTHER,
+  SPAWN_PLANET_3,
   LVL1_ASTEROID_1,
   LVL1_ASTEROID_2,
   LVL1_PLANET_NEBULA,
@@ -22,6 +24,8 @@ import {
   SMALL_INTERVAL,
   SPAWN_PLANET_1,
   SPAWN_PLANET_2,
+  shrinkingInitializers,
+  INVALID_TOO_CLOSE_SPAWN,
 } from './utils/WorldConstants';
 
 const { BigNumber: BN } = ethers;
@@ -34,13 +38,19 @@ describe('DarkForestShrink', function () {
 
     beforeEach(async function () {
       world = await fixtureLoader(shrinkingWorldFixture);
-        
+
+      const time = await getCurrentTime();
+
+      await world.contracts.core.setStartTime(time);
+      await world.contracts.core.setEndTime(time + 5000);
+      await world.contracts.core.adminSetWorldRadius(shrinkingInitializers.INITIAL_WORLD_RADIUS);
+
       await world.user1Core.initializePlayer(...makeInitArgs(SPAWN_PLANET_1, SPAWN_PLANET_1.distFromOrigin));
     });
 
-    it('should decrease in radius size over time', async function () {
+    it('should decrease radius size after move', async function () {
       const initRadius = await world.contracts.core.worldRadius();
-      await increaseBlockchainTime();
+      await increaseBlockchainTime(5);
 
       // Recall that universe will only shrink when a player makes a move.
       await world.user1Core.move(
@@ -51,46 +61,50 @@ describe('DarkForestShrink', function () {
       expect(currRadius.toNumber()).lessThan(initRadius.toNumber());
     });
 
-  });
-  
-  describe('in a shrinking universe : sequential', async function () {
-    let initialRadius: BigNumber;
-
-    before(async function () {
-      world = await fixtureLoader(shrinkingWorldFixture);
-        
-      await world.user1Core.initializePlayer(...makeInitArgs(SPAWN_PLANET_1, SPAWN_PLANET_1.distFromOrigin));
-    });
-
-    it('should conquer a planet', async function () {
-      await conquerUnownedPlanet(world, world.user1Core, SPAWN_PLANET_1, LVL1_PLANET_SPACE_FURTHER);
-      const star1Data = await world.contracts.core.planets(LVL1_PLANET_SPACE_FURTHER.id);
-      expect(
-        star1Data.owner
-      ).to.equal(world.user1.address);
-    });
-
-    it('should reject a move to a planet previously in radius', async function () {
-      const dist = 100;
-      const shipsSent = 50000;
-      const silverSent = 0;
-  
+    it('rejects a player spawning outside of middle ring', async function () {
+      const initRadius = await world.contracts.core.worldRadius();
       await expect(
-        world.user1Core.move(
-          ...makeMoveArgs(SPAWN_PLANET_1, LVL1_PLANET_SPACE_FURTHER, dist, shipsSent, silverSent)
-        )
-      ).to.be.revertedWith('Attempting to move out of bounds');
+        world.user2Core.initializePlayer(...makeInitArgs(SPAWN_PLANET_2, initRadius.toNumber()))
+      ).to.be.revertedWith("Init radius is too high");
+    });
+  
+    it('rejects a player spawning inside of middle ring', async function () {
+      await expect(
+        world.user2Core.initializePlayer(...makeInitArgs(SPAWN_PLANET_2, 0))
+      ).to.be.revertedWith("Init radius is too low");
     });
 
-    it('should reject a move from a planet previously in radius', async function () {
+    it('accepts a player spawning in middle ring and shrinks radius', async function () {
+      const initRadius = await world.contracts.core.worldRadius();
+      await expect(world.user2Core.initializePlayer(...makeInitArgs(SPAWN_PLANET_2, SPAWN_PLANET_2.distFromOrigin)))
+        .to.emit(world.contracts.core, 'PlayerInitialized')
+        .withArgs(world.user2.address, SPAWN_PLANET_2.id.toString());
+
+      const currRadius = await world.contracts.core.worldRadius();
+      expect(currRadius.toNumber()).lessThan(initRadius.toNumber());
+    });
+
+    it('radius cant go below minRadius', async function () {
+      const minRadius = (await world.contracts.core.gameConstants()).MIN_RADIUS;
+      const initRadius = (await world.contracts.core.worldRadius()).toNumber();
+
       await increaseBlockchainTime();
-      await conquerUnownedPlanet(world, world.user1Core, SPAWN_PLANET_1, LVL0_PLANET_POPCAP_BOOSTED);
-      const star1Data = await world.contracts.core.planets(LVL0_PLANET_POPCAP_BOOSTED.id);
-      expect(
-        star1Data.owner
-      ).to.equal(world.user1.address);
 
+      const currRadius = (await world.contracts.core.worldRadius()).toNumber();
+
+      expect(currRadius).to.equal(initRadius);
+
+      // player initializes before radius is shrunk. 
+      // hopefully this is taken care of by spawning in middle ring.
+      await expect(world.user2Core.initializePlayer(...makeInitArgs(SPAWN_PLANET_2, Math.floor(currRadius / 2))))
+        .to.emit(world.contracts.core, 'PlayerInitialized')
+        .withArgs(world.user2.address, SPAWN_PLANET_2.id.toString());
+
+      const finalRadius = await world.contracts.core.worldRadius();
+      console.log("finalRadius ", finalRadius.toNumber());
+      expect(finalRadius.toNumber()).to.equal(minRadius.toNumber());
     });
+  
 
   });
 });
